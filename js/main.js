b3e8479c920d37882ce7c590255f82a418ef9ab7
morkvd <markvdijken@gmail.com>
2016-10-26 15:13:12 +0200

    add score for weird transaction amounts

---
+++ js/main.js
@@ -0,0 +1,59 @@
+d3.csv('data/data.csv', function (data) {
+
+  // the average of all the transaction values (the amount of money paid)
+  const sumAmount = data.map((transaction) => +transaction.amount)
+                        .reduce((prevVal, currVal) => prevVal + currVal);
+  const meanAmount = sumAmount / data.length;
+
+  function calculateDeviation (transaction) {
+    const deviation = (transaction.amount / meanAmount) * 100;
+    const differenceFromMean = transaction.amount - meanAmount
+    return Object.assign(
+      {
+        deviation: deviation,
+        differenceFromMean: differenceFromMean,
+      },
+    transaction
+    );
+  }
+
+  const transactionsWithExtraCalculations = data.map(calculateDeviation)
+
+  function calculateVariance (transaction) {
+    return Math.pow(transaction.differenceFromMean, 2);
+  }
+
+  const varianceSum = transactionsWithExtraCalculations
+    .map(calculateVariance)
+    .reduce((previousValue, currentValue) => {
+      return previousValue + currentValue;
+    });
+
+  const standardDeviation = Math.sqrt(varianceSum / transactionsWithExtraCalculations.length)
+
+  console.log(d3.max(data.map((transaction) => +transaction.amount)));
+
+  const coolAssScale = d3.scaleLinear()
+                        .domain([meanAmount + standardDeviation, d3.max(data.map((transaction) => +transaction.amount))])
+                        .range([0, 20])
+                        .clamp(true)
+
+  console.log(standardDeviation);
+  console.log(meanAmount);
+
+  // if the value is 0 ALARM
+  // if the value has more than two numbers after the comma HALP
+  // else use scale 
+
+  function givePoints (transaction) {
+    return Object.assign(
+      {
+        score: transaction.amount == '0' ? 20 : coolAssScale(transaction.amount),
+      },
+      transaction
+    );
+  }
+
+  console.table(data.map(givePoints))
+
+});
