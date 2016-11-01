e2c9378d992a3c6a5132e728dbd8d7fd53cfe6a4
Giliam Verheide <giliamv@tamtam.nl>
2016-11-01 16:21:13 +0100

    Improved complete setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
 'use strict';
 
 var gulp = require('gulp');
+var gulpNunjucks = require('gulp-nunjucks-render');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['html', 'sass'], function() {
 
   browserSync.init({
-    server: "./"
+    server: "./build"
   });
 
-  gulp.watch("./sass/**/*.scss", ['sass']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./source/sass/**/*.scss", ['sass']);
+  gulp.watch("./source/html/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('html', function() {
+  return gulp.src('./source/html/*.html')
+    .pipe(gulpNunjucks({
+      path: ['./source/html'] // String or Array
+    }))
+    .pipe(gulp.dest('./build'));
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("./sass/*.scss")
+  return gulp.src("./source/sass/*.scss")
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'))
+    .pipe(gulp.dest('./build/assets/css'))
     .pipe(browserSync.stream());
 });
 
