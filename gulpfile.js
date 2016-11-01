cb2e7058ea68fdc6a286f641d24cc91b982a4488
Giliam Verheide <giliamv@tamtam.nl>
2016-11-01 14:30:27 +0100

    Server setup update

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./sass/**/*.scss", ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("./sass/*.scss")
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-  // Watch .js files
-  // gulp.watch('src/js/*.js', ['scripts']);
-  // Watch .scss files
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  // Watch image files
-  // gulp.watch('src/images/**/*', ['images']);
-});
-
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['serve']);
